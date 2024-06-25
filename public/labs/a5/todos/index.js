5ae3113659b3ea9a41a5e07d5ce497252960dfeb
gillianzili <ziliang.guo127@gmail.com>
2024-06-25 17:53:35 +0800

    todos

--- public/labs/a5/todos/index.js
+++ public/labs/a5/todos/index.js
@@ -1,10 +1,7 @@
 import TodoList from "./TodoList.js";
-
-$(document).ready(() => {
-    $('#wd-todo').append(`
-        <div class="container">
-            <h1>Todo example</h1>
-            ${TodoList()}
-        </div>
-    `);
-});
+$("#wd-todo").append(`
+   <div class="container">
+       <h1>Todo example</h1>
+       ${TodoList()}
+   </div>
+`);
