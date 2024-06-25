b28fb002c8cd032061240979307107dbb6ab50df
gillianzili <ziliang.guo127@gmail.com>
2024-06-25 13:49:16 +0800

    set up todos

--- public/labs/a5/index.js
+++ public/labs/a5/index.js
@@ -280,35 +280,84 @@ console.log(greeting2)
 const init = () => {
 
     console.log('Hello world from jQuery');
-
     const bindById = $('#bind-by-id');
-
     const bindByClass = $('.bind-by-class');
-
     console.log('Binding to DOM');
-
     console.log(bindById);
-
     console.log(bindByClass);
-
     const changeStyle = $('#change-style');
-
     const changeStyle2 = $('.change-style');
-
     changeStyle.css('color', 'red');
-
     changeStyle2.css('color', 'blue');
-
     const getIdAttr = $("#get-id-attr");
-
     const id = getIdAttr.attr('id');
 
     console.log(id);
 
     const setClassAttr = $("#set-class-attr");
+    setClassAttr.attr("class", "class-0");
+    const addClass1Example = $("#add-class-1");
+    addClass1Example.addClass("class-1");
+    const removeClass1Example = $("#remove-class-1");
+    removeClass1Example.removeClass('class-2');
 
-    setClassAttr.attr('class', 'class-0');
+    const hideMe = $("#hide-me");
+    hideMe.hide();
 
+    const showMe = $("#show-me");
+    showMe.show();
+
+    const newLineItem = $("<li>Line item 1</li>");
+    const anotherLineItem = $("<li>Line item 2</li>");
+
+    const ul = $("#append-new-elements");
+    ul.append(newLineItem);
+    ul.append(anotherLineItem);
+
+    const removeLi = $("#remove-this");
+    const emptyUl = $("#empty-this");
+    removeLi.remove();
+    emptyUl.empty();
+
+    const changeThisText = $("#change-this-text");
+    const changeThisHtml = $("#change-this-html");
+    changeThisText.html('New text');
+    changeThisHtml.html(`
+   <li>Line item A</li>
+   <li>Line item B</li>
+   <li>Line item C</li>
+`);
+
+    const child2 = $("#child-2");
+    const parent1 = child2.parents("#parent");
+    parent1
+        .css('background-color', 'red')
+        .css('color', 'white');
+    const parent = $("#parent");
+    const child = parent.find("#child-2");
+    child.css('background-color', 'blue')
+    const handleClick = () => console.log('Handle click');
+    const clickable = $('.clickable');
+    clickable.click(handleClick);
+
+    const handleEventTarget = (event) => {
+        const target = event.target;
+        console.log(target);
+        $(target)
+            .css('background-color', 'blue')
+            .css('color', 'white');
+    }
+    const eventTarget = $("#event-target");
+    eventTarget.click(handleEventTarget);
+
+    let hideBtn, showBtn, hideShowHeader;//声明3个变量
+    hideBtn = $('#hide');//使用 jQuery 选择器 $('#hide')，将 id 为 hide 的元素赋值给 hideBtn 变量
+    showBtn = $('#show');//赋值
+    hideShowHeader = $('#hide-show');//赋值
+    const hideHandler = () => {hideShowHeader.hide();}//声明箭头函数，调用 hideShowHeader.hide()，这会隐藏 id 为 hide-show 的元素。
+    const showHandler = () => {hideShowHeader.show();}
+    hideBtn.click(hideHandler);//为 hideBtn（即 id 为 hide 的元素）添加了一个点击事件监听器。当 hideBtn 被点击时，将执行 hideHandler 函数，从而隐藏 hideShowHeader
+    showBtn.click(showHandler);
 
 }
 $(init);
