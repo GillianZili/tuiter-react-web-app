1f9f462820da5b13a5dd0b52b9d816c45f0ff9be
gillianzili <ziliang.guo127@gmail.com>
2024-06-25 17:39:28 +0800

    todo list

--- public/labs/a5/todos/TodoList.js
+++ public/labs/a5/todos/TodoList.js
@@ -1,10 +1,14 @@
+import TodoItem from "./TodoItem.js";
+import todos from "./todos.js";
+
 const TodoList = () => {
-    return(`
+    return `
         <ul>
-            <li>Buy milk</li>
-            <li>Pickup the kids</li>
-            <li>Walk the dog</li>
+            ${todos.map(todo => {
+                return TodoItem(todo);
+            }).join('')}
         </ul>
-    `);
- }
- export default TodoList;
+    `;
+};
+
+export default TodoList;
--- public/labs/a5/todos/index.js
+++ public/labs/a5/todos/index.js
@@ -1,14 +1,10 @@
-$('#wd-todo').append(`
-    <div class="container">
-        <h1>Todo example</h1>
-    </div>
- `);
-
 import TodoList from "./TodoList.js";
-$('#wd-todo').append(`
-   <div class="container">
-       <h1>Todo example</h1>
-       ${TodoList()}
-   </div>
-`);
 
+$(document).ready(() => {
+    $('#wd-todo').append(`
+        <div class="container">
+            <h1>Todo example</h1>
+            ${TodoList()}
+        </div>
+    `);
+});
