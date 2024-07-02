24c48300366867acb004c8d61224b1fc43e130da
gillianzili <ziliang.guo127@gmail.com>
2024-07-02 16:15:03 +0800

    a6/src/tuiter/navigation-sidebar/index.js change path

--- src/tuiter/navigation-sidebar/index.js
+++ src/tuiter/navigation-sidebar/index.js
@@ -3,12 +3,15 @@ import { Link } from "react-router-dom";
 import { useLocation } from "react-router";
 import "../explore/index.css";
 
-const NavigationSidebar = ({ active = 'Explore' }) => {
+const NavigationSidebar = ({ }) => {
+    const { pathname } = useLocation();//使用React Router中的 useLocation() 钩子来获取当前页面的URL路径信息
+    const paths = pathname.split('/')
+    const active = paths[2];
     return (
         <div className="list-group">
             <a className="list-group-item">Tuiter</a>
-            <Link to="/tuiter/home" className={`list-group-item ${active === 'home'?' myactive':''}`}>Home</Link>
-            <Link to="/tuiter/explore" className={`list-group-item ${active === 'Explore'?' myactive':''}`}>Explore</Link>
+            <Link to="/tuiter/home" className={`list-group-item ${active === 'home' ? ' myactive' : ''}`}>Home</Link>
+            <Link to="/tuiter/explore" className={`list-group-item ${active === 'explore' ? ' myactive' : ''}`}>Explore</Link>
             <Link to="/" className="list-group-item">Labs</Link>
             <a className={`list-group-item${active === 'Notifications' ? ' myactive' : ''}`}>Notifications</a>
             <a className={`list-group-item${active === 'Messages' ? ' myactive' : ''}`}>Messages</a>
